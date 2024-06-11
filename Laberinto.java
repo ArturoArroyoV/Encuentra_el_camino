50c3894715e6a4170cd0fdcfa13866218ab179b1
Diego Miguel Aguirre Toribio <diego.aguirret@usil.pe>
2024-06-11 00:17:27 -0500

    Add files via upload
    Se creó la clase principal, la clase laberinto y la clase personaje.

---
+++ Laberinto.java
@@ -0,0 +1,5 @@
+public class Laberinto {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Personaje.java
@@ -0,0 +1,5 @@
+public class Personaje {
+    public static void main(String[] args) {
+
+    }
+}
