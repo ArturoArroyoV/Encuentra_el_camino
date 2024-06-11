26d23550c62c83a93631df42b70bcf4cb5432c8c
Diego Miguel Aguirre Toribio <diego.aguirret@usil.pe>
2024-06-11 00:44:32 -0500

    Update Laberinto.java

--- Laberinto.java
+++ Laberinto.java
@@ -1,5 +1,11 @@
 public class Laberinto {
-    public static void main(String[] args) {
+    private final int[][] laberinto;
+    private final int filas, columnas;
+    private final int TAMANO = 50;
 
+    public Laberinto(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.laberinto = new int[filas][columnas];
     }
 }
