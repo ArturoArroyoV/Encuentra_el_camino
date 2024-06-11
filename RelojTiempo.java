47f38ba27ba15dd4fbc81f6849ec5779ae756924
Leandro1901 <leandro.ruizv@usil.pe>
2024-06-11 00:35:25 -0500

    Add files via upload

---
+++ RelojTiempo.java
@@ -0,0 +1,13 @@
+
+public class RelojTiempo {
+    
+    private int x,y;
+    private final int TAMANO = 20;
+
+    public RelojTiempo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+}
