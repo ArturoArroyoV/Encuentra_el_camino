466bb07c797b6d09b4c5034ad8a301ef9e5f97ef
ArturoArroyoV <arroyoarturo921@gmail.com>
2024-06-11 22:55:35 -0500

    TEMPORIZADOR FINAL?

--- Temporizador.java
+++ Temporizador.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -6,8 +5,20 @@ public class Temporizador {
     private int tiempoRestante;
     private Timer timer;
 
-    public Temporizador(int tiempoInicial) {
+ public Temporizador(int tiempoInicial) {
         this.tiempoRestante = tiempoInicial;
         this.timer = new Timer();
         iniciarTemporizador();
+    }
+ private void iniciarTemporizador() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (tiempoRestante > 0) {
+                    tiempoRestante--;
+                } else {
+                    timer.cancel();
+                }
+            }
+        }, 1000, 1000);
     }
