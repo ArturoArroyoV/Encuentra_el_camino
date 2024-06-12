16c01a45a27a99ac659dbc7003161ac78e5f0034
Leandro1901 <leandro.ruizv@usil.pe>
2024-06-11 20:12:34 -0500

    Update AFD.java

--- AFD.java
+++ AFD.java
@@ -6,4 +6,13 @@ public class AFD {
 public AFD( int [][] tablaTransiciones) {
     this.tablaTransiciones = tablaTransiciones;
     this.estadoActual = 0;
+    }
+public void transicionar(int entrada){
+    estadoActual = tablaTransiciones[estadoActual][entrada];
+}
+public void dibujar (Graphics g){
+    //Dibujar los estados y transiciones del AFD
+    g.setColor(Color.RED);
+    g.drawString("Estado actual: " + estadoActual, 10, 20);
+    }
 }
