18a474991f46ced16c3b5e3c89a33b52ed9fb08c
ArturoArroyoV <arroyoarturo921@gmail.com>
2024-06-29 16:39:01 -0500

    Update Temporizador.java

--- Temporizador.java
+++ Temporizador.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -5,12 +6,13 @@ public class Temporizador {
     private int tiempoRestante;
     private Timer timer;
 
- public Temporizador(int tiempoInicial) {
+    public Temporizador(int tiempoInicial) {
         this.tiempoRestante = tiempoInicial;
         this.timer = new Timer();
         iniciarTemporizador();
     }
- private void iniciarTemporizador() {
+
+    private void iniciarTemporizador() {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -18,7 +20,18 @@ public class Temporizador {
                     tiempoRestante--;
                 } else {
                     timer.cancel();
+                    // Lógica cuando el tiempo se acaba (e.g., reiniciar nivel)
                 }
             }
         }, 1000, 1000);
     }
+
+    public void agregarTiempo(int segundos) {
+        tiempoRestante += segundos;
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.RED);
+        g.drawString("Tiempo: " + tiempoRestante, 10, 30);
+    }
+}
