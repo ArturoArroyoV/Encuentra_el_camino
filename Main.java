a3b50774a169d1cef0cfa387b1333988d46fb11c
ArturoArroyoV <arroyoarturo921@gmail.com>
2024-06-29 19:32:20 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    
+        public static void main(String[] args) {
+        char[][] laberinto = {
+            // Cargar el laberinto desde un archivo
+        };
+        JFrame frame = new JFrame("Juego del Laberinto");
+        JuegoLaberinto juego = new JuegoLaberinto(laberinto);
+        frame.add(juego);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
+}
